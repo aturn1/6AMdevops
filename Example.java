620904f214b6e27b49c36005b9ff45bd4b50c123
prashanth <projects2488@gmail.com>
2025-05-26 06:42:08 +0530

    c4 - 10k

--- Example.java
+++ Example.java
@@ -5,5 +5,6 @@ public static void main(String[] args)
 System.out.println("1000 lines");
 System.out.println("2000 lines");
 System.out.println("5000 lines");
+System.out.println("10K lines");
 }
 }
