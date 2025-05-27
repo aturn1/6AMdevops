a02c8826720393f7c27002511ca5672b8a034477
aturn1 <naresh.batch001@gmail.com>
2025-05-27 06:26:56 +0530

    Update Example.java

--- Example.java
+++ Example.java
@@ -7,5 +7,8 @@ System.out.println("2000 lines");
 System.out.println("5000 lines");
 System.out.println("10K lines");
 System.out.println("25k lines of code");
+System.out.println("Hello Srinivas");
+  System.out.println("Hello Pavani");
+  System.out.println("Hello Ramya");
 }
 }
